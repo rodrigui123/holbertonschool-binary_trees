76cafc1cd96d758b3361d7263f9aa7c9b7c87ef4
Rodrigo <5093@holbertonschool.com>
2022-08-31 13:31:17 -0700

    prototypes

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
