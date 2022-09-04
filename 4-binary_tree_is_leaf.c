26093a8c58b85e3232d871502f097a65da3decba
Romina Guimaraens <5071@holbertonschool.com>
2022-09-04 15:35:50 -0700

    task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -11,13 +11,8 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	{
 		return (0);
 	}
-	if (node->parent)
-	{
-		if (!node->left && !node->right)
-			return (1);
-		else
-			return (0);
-	}
+	if (!node->left && !node->right)
+		return (1);
 	else
 		return (0);
 }
