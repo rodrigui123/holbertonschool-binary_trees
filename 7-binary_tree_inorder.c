887c991ffc375fe9c66c0f088955ef73a19f528d
Rodrigo <5093@holbertonschool.com>
2022-09-04 16:59:46 -0700

    t7 n is missing in the output

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -11,6 +11,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 	if (tree->left)
 		binary_tree_inorder(tree->left, func);
+	func(tree->n);
 	if (tree->right)
 		binary_tree_inorder(tree->right, func);
 }
