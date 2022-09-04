fc5841e02e7fc0d1976d8c8b873be39118bf49ab
Romina Guimaraens <5071@holbertonschool.com>
2022-09-04 15:34:12 -0700

    task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+#include <stdio.h>
+/**
+ * binary_tree_is_leaf - checks if node is leaf
+ * @node: node to check
+ * Return:1 if leaf, 0 otherwise
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+	{
+		return (0);
+	}
+	if (node->parent)
+	{
+		if (!node->left && !node->right)
+			return (1);
+		else
+			return (0);
+	}
+	else
+		return (0);
+}
