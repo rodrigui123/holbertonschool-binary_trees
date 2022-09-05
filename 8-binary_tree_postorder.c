4455dd3e8f835760719c65df3c3c2839d2cd4f8a
Rodrigo <5093@holbertonschool.com>
2022-09-04 18:05:20 -0700

    t8 changing order of func

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -11,8 +11,8 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 	if (tree->left)
 		binary_tree_postorder(tree->left, func);
-	func(tree->n);
 	if (tree->right)
 		binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
 
